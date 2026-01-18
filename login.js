6f3b54cab1846a043bec4e6ea3c56fba195b7cd7
SASHI117 <122377510+SASHI117@users.noreply.github.com>
2026-01-18 09:21:48 +0530

    Update login.js

--- login.js
+++ login.js
@@ -7,12 +7,11 @@ form.addEventListener("submit", (e) => {
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  // TEMP credentials
+  // DEMO CREDENTIALS
   if (username === "admin" && password === "admin123") {
     localStorage.setItem("isLoggedIn", "true");
     window.location.href = "index.html";
   } else {
-    errorMsg.textContent = "Incorrect username or password";
     errorMsg.classList.remove("hidden");
   }
 });
